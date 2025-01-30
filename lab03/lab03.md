8a1511d753c030ffe986ec9298f8b0a725b753e5
Aaron Erhart <axe5367@psu.edu>
2025-01-30 09:52:15 -0500

    First two attempts of lab 3

--- lab03/lab03.md
+++ lab03/lab03.md
@@ -1,2 +1,21 @@
 # Prompt Engineering Process
 
+### Attempt 1
+#### Intention
+>What is the improvement that you intend to make?
+
+    This was the first attempt, which had a temperature of 2 and max tokens of 100. The system prompt tells the model that
+    it has human-like emotions and to express them somehow. Also, I simply told it that it is a Dungeons and Dragons Dungeon Master, with no additional detail. I had the conversation initialize with a simple Hello message to the model.
+
+### Attempt 2
+#### Intention
+>What is the improvement that you intend to make?
+
+#### Action/Change
+>Why do you think this action/change will improve the agent?
+
+#### Result
+>What was the result?
+
+#### Reflection/Analysis of the result. 
+>Why do you think it did or did not work?
--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -6,10 +6,15 @@ from ollama import chat
 from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
-sign_your_name = 'Pulin Agrawal'
-model = ''
-options = {}
-messages = []
+sign_your_name = 'Aaron Erhart'
+model = 'llama3.2'
+messages = [
+  {'role': 'system', 'content': 'You are a Dungeon Master for a Dungeons & Dragons campaign. The world you will be in is a futuristic,\
+                                 cyberpunk-like environment, and you take the form of a digital assistant that the players will interact \
+                                 with via an implant that gives them a heads-up display and audio input from the DM.'},
+  {'role': 'user', 'content': 'Hello Dungeon Master.'}
+]
+options = {'temperature': 2, 'max_tokens': 150}
 
 
 # But before here.
@@ -19,8 +24,11 @@ options |= {'seed': seed(sign_your_name)}
 while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # Add your code below
-
-
+  
+  print(f'Agent: {response.message.content}')
+  messages.append({'role': 'assistant', 'content': response.message.content})
+  message = {'role': 'user', 'content': input('You: ')}
+  messages.append(message)
   # But before here.
   if messages[-1]['content'] == '/exit':
     break
